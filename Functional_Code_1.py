8e5d6fcc8e05a6ecf52651e062d80ebf17644099
Fredrick Omondi Okello <okellofredrickomondi@gmail.com>
2017-01-17 15:24:49 +0300

    Andela Prime numbers functions
    get prime number 'n'

--- Functional_Code_1.py
+++ Functional_Code_1.py
@@ -6,13 +6,13 @@ result = []
 x=2
 while len(result) in range(count):
     i=2
-    flag=0
+    flag_n=0
     for i in range(2,x):
         if x%i == 0:
-            flag+=1
+            flag_n+=1
             break
         i=i+1
-    if flag == 0:
+    if flag_n == 0:
         result.append(x)
     x+=1
 pass
